3573136933f10536e0261f2f6a37341ce00c2a46
ong_osmond <ong.osmond@yahoo.com>
2020-09-19 23:41:27 +0800

    Updates

---
+++ precache-manifest.97f36feb41ff43d90d78a8bb1ccba9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6784927abd3f4446fbce8b1a8cf593a",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "45266f84f0fbfe0a0c6b",
+    "url": "/employee-directory/static/css/main.14325ab3.chunk.css"
+  },
+  {
+    "revision": "37eded062550cc4e6a98",
+    "url": "/employee-directory/static/js/2.77858d5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.77858d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45266f84f0fbfe0a0c6b",
+    "url": "/employee-directory/static/js/main.f989406b.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
